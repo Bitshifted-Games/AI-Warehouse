588ee45a2d0157137096576659755e87786dcdad
Mick Evans <mevans.bitshiftedgames@gmail.com>
2019-01-11 13:36:46 -0500

    Added ToUshort extension methods
    feat: floats and ints can now be converted directly to ushorts by simply calling ToUshort() on them

---
+++ Assets/Bitshifted Games/Utility/Classes/Ushort_Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Ushort_Extensions
+{
+    public static ushort AsUshort (this float toConvert)
+    {
+        return 
+            (ushort)Mathf.Lerp ( float.MinValue, float.MaxValue, toConvert );
+    }
+    public static ushort AsUshort (this int toConvert )
+    {
+        return
+            (ushort)Mathf.Lerp ( int.MinValue, int.MaxValue, toConvert );
+    }
+}

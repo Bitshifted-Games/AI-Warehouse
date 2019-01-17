c92ea013d5dbc4a68feb64aaf2e6307501f3c858
Mick Evans <mevans.bitshiftedgames@gmail.com>
2019-01-16 19:35:09 -0500

    Major refactor of Utility dir
    Removed CargoDimensions and Ushort_Extensions
    Added LevelGrid and ObjectPlacer

---
+++ Assets/Bitshifted Games/Utility/Scripts/LevelGrid.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace BitshiftedGames.Utility
+{
+    public class LevelGrid : MonoBehaviour
+    {
+        [SerializeField] Vector2 gridSize = new Vector2 ( 100, 60 );
+        [SerializeField] private float size = 1f;
+
+        public Vector3 GetNearestPointOnGrid ( Vector3 position )
+        {
+            position -= transform.position;
+
+            int xCount = Mathf.RoundToInt ( position.x / size );
+            int yCount = Mathf.RoundToInt ( position.y / size );
+            int zCount = Mathf.RoundToInt ( position.z / size );
+
+            Vector3 result = new Vector3 (
+                xCount * size,
+                yCount * size,
+                zCount * size );
+
+            result += transform.position;
+
+            return result;
+        }
+
+        private void OnDrawGizmos ()
+        {
+            Gizmos.color = Color.yellow;
+            for ( float x = 0; x < gridSize.x; x += size )
+            {
+                for ( float z = 0; z < gridSize.y; z += size )
+                {
+                    var point = GetNearestPointOnGrid ( new Vector3 ( x, 0f, z ) );
+                    Gizmos.DrawSphere ( point, 0.1f );
+                }
+
+            }
+        }
+    }
+}
---
+++ Assets/Bitshifted Games/Utility/Scripts/ObjectPlacer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace BitshiftedGames.Utility
+{
+    public class ObjectPlacer : MonoBehaviour
+    {
+        private LevelGrid grid;
+
+        public GameObject objectToPlace;
+
+        private void Awake ()
+        {
+            grid = FindObjectOfType<LevelGrid> ();
+        }
+
+        private void Update ()
+        {
+            if ( Input.GetMouseButtonDown ( 0 ) )
+            {
+                Ray ray = Camera.main.ScreenPointToRay ( Input.mousePosition );
+
+                if ( Physics.Raycast ( ray, out RaycastHit hitInfo ) )
+                {
+                    PlaceCubeNear ( hitInfo.point );
+                }
+            }
+        }
+
+        private void PlaceCubeNear ( Vector3 clickPoint )
+        {
+            Debug.Log ( "Placing object" );
+            var finalPosition = grid.GetNearestPointOnGrid ( clickPoint );
+            GameObject go = Instantiate ( objectToPlace, finalPosition, Quaternion.identity, grid.transform.parent );
+            //GameObject.CreatePrimitive ( PrimitiveType.Cube ).transform.position = finalPosition;
+
+            //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = nearPoint;
+        }
+    }
+}
